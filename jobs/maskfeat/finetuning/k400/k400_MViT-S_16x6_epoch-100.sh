6fc6d4df7dcd6b8611abdcd222e7074211335673
obrookes <dl18206@bristol.ac.uk>
2023-04-05 16:09:45 +0100

    job scripts for k400 and scratch finetuning

---
+++ jobs/maskfeat/finetuning/k400/k400_MViT-S_16x6_epoch-100.sh
@@ -0,0 +1,23 @@
+#!/bin/bash    
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=devel
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=32GB
+#SBATCH --job-name=maskfeat_ft_k400_epoch-100
+#SBATCH --output=%x-%j.out
+
+module load cuda/11.2
+
+source ~/.bashrc
+conda activate slowfast_torch113_cu116 
+
+cd ~/SlowFast
+
+export PYTHONWARNINGS="ignore"
+
+python tools/run_net.py --cfg configs/masked_ssl/MaskFeat/k400/k400_MVITv2_S_16x4_FT_epoch-100.yaml
+
