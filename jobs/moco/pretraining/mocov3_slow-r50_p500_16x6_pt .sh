88cc9964057cbd893c2445532583050e96d7d605
obrookes <dl18206@bristol.ac.uk>
2023-04-05 20:33:38 +0100

    cropped experiment configs

---
+++ jobs/moco/pretraining/mocov3_slow-r50_p500_16x6_pt .sh
@@ -0,0 +1,23 @@
+#!/bin/bash    
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=devel
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=256GB
+#SBATCH --job-name=mocov3_slow-r50_p500_16x24_pt
+#SBATCH --output=%x-%j.out
+
+module load cuda/11.2
+
+source ~/.bashrc
+conda activate slowfast_torch113_cu116 
+
+cd ~/SlowFast
+
+export PYTHONWARNINGS="ignore"
+
+python tools/run_net.py --cfg configs/contrastive_ssl/MoCo/pretraining/MoCo_K400_SlowR50_Cropped_PanAf500_16x6.yaml
+
