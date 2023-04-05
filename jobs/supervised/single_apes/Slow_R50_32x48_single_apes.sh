1ee139dbafc010b3105eb57dbdfef3a57671b199
obrookes <dl18206@bristol.ac.uk>
2023-04-05 14:33:28 +0100

    single ape mvit2 and slowr50

---
+++ jobs/supervised/single_apes/Slow_R50_32x48_single_apes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=devel
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=128GB
+#SBATCH --job-name=slow_r50_single-apes_32x48
+#SBATCH --output=%x-%j.out
+
+
+module load cuda/11.2
+
+source ~/.bashrc
+conda activate slowfast_torch113_cu116 
+
+cd ~/SlowFast
+
+export PYTHONWARNINGS="ignore"
+
+python tools/run_net.py --cfg configs/Kinetics/single_ape/Slow_R50_32x48_SINGLE_APES.yaml
