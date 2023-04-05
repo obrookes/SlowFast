12091bcc3174737d02d3fcb293045ded9e5fce07
obrookes <dl18206@bristol.ac.uk>
2023-04-05 19:37:01 +0100

    scatch slow configs

---
+++ jobs/moco/finetuning/scratch/Slow_16x6_R50_epoch-250.sh
@@ -0,0 +1,23 @@
+#!/bin/bash    
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=devel
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=32GB
+#SBATCH --job-name=moco_ft_epoch-250
+#SBATCH --output=%x-%j.out
+
+module load cuda/11.2
+
+source ~/.bashrc
+conda activate slowfast_torch113_cu116 
+
+cd ~/SlowFast
+
+export PYTHONWARNINGS="ignore"
+
+python tools/run_net.py --cfg configs/contrastive_ssl/MoCo/finetuning/scratch/Slow_16x6_R50_epoch-250.yaml
+
